9afae38423ae530c303774d1600e8af71f36e6dd
deepanraj <deepanrajs.21aid@kongu.edu>
2024-02-13 22:30:36 +0530

    data transformation is done

---
+++ src/components/data_transformation.py
@@ -0,0 +1,101 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.logger import logging
+from src.exception import CustumException
+from src.utils import save_object
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_path=os.path.join("artifacts","preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transform_config=DataTransformationConfig()
+    
+    def get_data_transformaer_obj(self):
+        try:
+            numerical_columns=["writing_score","reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+            num_pipeline=Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="median")),
+                    ("scalar",StandardScaler())
+                ]
+            )
+            cat_pipeline=Pipeline(
+                steps=[
+                    ("imputer",SimpleImputer(strategy="most_frequency")),
+                    ("one_hot_encoder",OneHotEncoder()),
+                    ("scaler",StandardScaler())
+                    
+                ]
+            )
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+            
+            
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_columns),
+                    ("cat_pipeline",cat_pipeline,categorical_columns)
+                ]
+            )
+            return preprocessor
+        except Exception as e:
+            raise CustumException(e,sys)   
+        
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessing object") 
+                               
+            preprocessor_obj=self.get_data_transformaer_obj()
+            
+            target_column_name="math_score" 
+            numerical_columns=["writing_score","reading_score"]
+
+            input_feature_train_df = train_df.drop(columns=target_column_name,axis=1)
+            target_feature_train_df=train_df[target_column_name] 
+            
+            input_feature_test_df = test_df.drop(columns=target_column_name,axis=1)
+            target_feature_test_df=test_df[target_column_name] 
+            
+            train_arr=np.c_[
+                input_feature_train_df,np.array(target_feature_train_df)
+            ]
+            test_arr=np.c_[
+                input_feature_test_df,np.array(target_feature_test_df)
+            ]
+            
+            save_object(
+                file_path=self.data_transform_config.preprocessor_obj_path,
+                obj=preprocessor_obj
+            )
+            logging.info(f"Saved preprocessing object.")
+            return(
+                train_arr,
+                test_arr,
+                self.data_transform_config.preprocessor_obj_path,
+            )
+        except Exception as e:
+            raise CustumException(e,sys)
+
+# if __name__=="__main__": 
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from src.exception import CustumException
+import dill
+import pickle
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj) 
+    except Exception as e:
+        raise CustumException(e,sys)
